5e54789dc9dd9e9be3a4f6da2740b71568c72086
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2020-09-01 21:55:59 -0300

    Put hover inside a buildFHSUserEnv
    Based on this PR: https://github.com/NixOS/nixpkgs/pull/80075
    
    I exported the `go` and `gcc` packages so it was available inside the
    FHS env and also fixed the X11 libraries, and this seems to do the
    trick.
    
    There is no more reason to wrapper with CPATH, and the only reason we
    keep LD_LIBRARY_PATH is because hover is built outside the
    buildFHSUserEnv so it wouldn't found the libraries otherwise.

--- pkgs/hover/default.nix
+++ pkgs/hover/default.nix
@@ -1,81 +1,82 @@
 { lib, buildGoModule, buildFHSUserEnv, pkgconfig, fetchFromGitHub,
-  stdenv, writeScript, xorg, libglvnd, addOpenGLRunpath, makeWrapper,
-  gcc, go, flutter }:
+stdenv, writeScript, xorg, libglvnd, addOpenGLRunpath, makeWrapper,
+gcc, go, flutter }:
 
 let
   pname = "hover";
   version = "0.43.0";
 
   libs = with xorg; [
-    libXi
-    libXxf86vm
-    libglvnd.dev
     libX11.dev
     libXcursor.dev
+    libXext.dev
+    libXi.dev
     libXinerama.dev
     libXrandr.dev
     libXrender.dev
+    libXfixes.dev
+    libXxf86vm
+    libglvnd.dev
     xorgproto
   ];
-in buildGoModule rec {
-  inherit pname version;
+  hover = buildGoModule rec {
+    inherit pname version;
 
-  meta = with stdenv.lib; {
-    description = "A build tool to run Flutter applications on desktop";
-    homepage = "https://github.com/go-flutter-desktop/hover";
-    license = licenses.bsd3;
-    platforms = platforms.linux ++ platforms.darwin;
+    meta = with stdenv.lib; {
+      description = "A build tool to run Flutter applications on desktop";
+      homepage = "https://github.com/go-flutter-desktop/hover";
+      license = licenses.bsd3;
+      platforms = platforms.linux ++ platforms.darwin;
+    };
+
+    subPackages = [ "." ];
+
+    vendorSha256 = "1wr08phjm87dxim47i8449rmq5wfscvjyz65g3lxmv468x209pam";
+
+    src = fetchFromGitHub {
+      rev = "v${version}";
+      owner = "go-flutter-desktop";
+      repo = pname;
+      sha256 = "0iw6sxg86wfdbihl2hxzn43ppdzl1p7g5b9wl8ac3xa9ix8759ax";
+    };
+
+    nativeBuildInputs = [ addOpenGLRunpath makeWrapper ];
+
+    buildInputs = libs;
+
+    checkRun = false;
+
+    patches = [
+      ./fix-assets-path.patch
+    ];
+
+    postPatch = ''
+      sed -i 's|@assetsFolder@|'"''${out}/share/assets"'|g' internal/fileutils/assets.go
+    '';
+
+    postInstall = ''
+      mkdir -p $out/share
+      cp -r assets $out/share/assets
+      chmod -R a+rx $out/share/assets
+
+      wrapProgram "$out/bin/hover" \
+      --prefix LD_LIBRARY_PATH : ${stdenv.lib.makeLibraryPath libs}
+    '';
+
+    postFixup = ''
+      addOpenGLRunpath $out/bin/hover
+    '';
   };
 
-  subPackages = [ "." ];
-
-  vendorSha256 = "1wr08phjm87dxim47i8449rmq5wfscvjyz65g3lxmv468x209pam";
-
-  src = fetchFromGitHub {
-    rev = "v${version}";
-    owner = "go-flutter-desktop";
-    repo = pname;
-    sha256 = "0iw6sxg86wfdbihl2hxzn43ppdzl1p7g5b9wl8ac3xa9ix8759ax";
-  };
-
-  nativeBuildInputs = [ addOpenGLRunpath makeWrapper ];
-
-  propagatedBuildInputs = [
+in buildFHSUserEnv rec {
+  name = pname;
+  targetPkgs = pkgs: [
     flutter
     gcc
     go
+    hover
     pkgconfig
   ] ++ libs;
 
-  checkRun = false;
-
-  patches = [
-    ./fix-assets-path.patch
-  ];
-
-  postPatch = ''
-    sed -i 's|@assetsFolder@|'"''${out}/share/assets"'|g' internal/fileutils/assets.go
-  '';
-
-  postInstall = ''
-    mkdir -p $out/share
-    cp -r assets $out/share/assets
-    chmod -R a+rx $out/share/assets
-
-    wrapProgram "$out/bin/hover" \
-      --prefix CPATH : ${xorg.libX11.dev}/include:${xorg.libXcursor.dev}/include:${xorg.libXi.dev}/include:${xorg.libXinerama.dev}/include:${xorg.libXrandr.dev}/include:${xorg.libXxf86vm.dev}/include:${xorg.libXext.dev}/include \
-      --prefix LD_LIBRARY_PATH : ${stdenv.lib.makeLibraryPath [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXrandr
-        xorg.libXxf86vm
-        xorg.libXext
-      ]}
-  '';
-
-  postFixup = ''
-    addOpenGLRunpath $out/bin/hover
-  '';
+  runScript = "hover";
 }
