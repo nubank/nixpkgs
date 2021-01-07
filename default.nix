aeaacce4596f5b974afef21352da1c5b8d4e16c7
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-01-07 17:56:00 -0300

    Bump nixpkgs-unstable

--- default.nix
+++ default.nix
@@ -3,15 +3,10 @@ let
   unstable = import (builtins.fetchTarball {
     # When bumping, use the last commit from https://github.com/NixOS/nixpkgs/tree/nixpkgs-unstable
     # So we can use Hydra cache when possible
-    url = "https://github.com/nixos/nixpkgs/archive/2080afd039999a58d60596d04cefb32ef5fcc2a2.tar.gz";
+    url = "https://github.com/nixos/nixpkgs/archive/479867700baa7ced259c979a0f724915c4d46d12.tar.gz";
     # Use fakeSha256 to generate a new sha256 when updating, i.e.:
     # sha256 = super.stdenv.lib.fakeSha256;
-    sha256 = "0i677swvj8fxfwg3jibd0xl33rn0rq0adnniim8jnp384whnh8ry";
-  }) {};
-  # TODO: Remove when merged: https://github.com/NixOS/nixpkgs/pull/108328
-  clojureLspBump = import (builtins.fetchTarball {
-    url = "https://github.com/ericdallo/nixpkgs/archive/b12b7f96ad7ccdf057cbcd5d34b57a854c601709.tar.gz";
-    sha256 = "0g4fjs7ydpw4r4a2fmccr82pifc1vwvca590zr5bjnmalfwrbhvp";
+    sha256 = "0dwci1sw81qn0b0cnksw3qf9lfhyz0rfvs14l3yh9iq6qfcs2aq7";
   }) {};
   callPackage = unstable.pkgs.callPackage;
 in
@@ -69,7 +64,7 @@ in
       babashka
       clj-kondo
       clojure
-      clojureLspBump.clojure-lsp
+      clojure-lsp
       leiningen
     ];
 
