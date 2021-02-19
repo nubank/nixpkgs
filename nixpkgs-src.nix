8a5a50eca95e8bed5b7669cc95bf1d0db3c97211
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-02-19 12:19:25 -0300

    Bump nixpkgs-unstable

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,10 +1,10 @@
 { lib, system, ... }:
 
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/f5daa8ab31fb910712b3e1e71ae46818bc9e33b3.tar.gz";
+  url = "https://github.com/nixos/nixpkgs/archive/c039c62aae24899860719c8a669ac94ec5daa99d.tar.gz";
   # Use fakeSha256 to generate a new sha256 when updating, i.e.:
   # sha256 = lib.fakeSha256;
-  sha256 = "0sa96nmbs37fix7xxxzj3dmryi515arnzicfg71qhkp45gn44dzq";
+  sha256 = "0gsid8wadkhy9yyajycg22csxix7m9fjl8qdpdqzm53la1nhwj26";
 }) {
   config = { allowUnfree = true; };
   inherit system;
