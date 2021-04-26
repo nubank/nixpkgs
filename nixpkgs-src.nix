5b46128567f041d5ff730f6c5d1536ce25eda5ed
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-04-26 11:20:57 -0300

    Bump nixpkgs again
    Babashka is missing from the previous bump (sorry, messed up).

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,10 +1,10 @@
 { lib, stdenv, ... }:
 
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/64be38f31fa76963ec31d361f0b797f0cca9d31f.tar.gz";
+  url = "https://github.com/nixos/nixpkgs/archive/32f7980afb5e33f1e078a51e715b9f102f396a69.tar.gz";
   # Use fakeSha256 to generate a new sha256 when updating, i.e.:
   # sha256 = lib.fakeSha256;
-  sha256 = "11bzji77s1zkww0qfkpqp5rd6np1y7c3syp4x3627w330ap94k8x";
+  sha256 = "02k9xnkrcmkr21b8pycmf1rbbnzh2x5i2wj9wmlghijfvpjqdn27";
 }) {
   config = { allowUnfree = true; };
   system = stdenv.system;
