9e9e0d497021d6d5b2dee3e560988c094816db96
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2021-06-08 13:39:47 -0300

    Bump nixpkgs-src, Python and leiningen

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,8 +1,8 @@
 { stdenv, ... }:
 
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/efee454783c5c14ae78687439077c1d3f0544d97.tar.gz";
-  sha256 = "1qk4g8rav2mkbd6y2zr1pi3pxs4rwlkpr8xk51m0p26khprxvjaf";
+  url = "https://github.com/nixos/nixpkgs/archive/12e7af1cfb3ab5bbbbd1d213a0b17c11ce9d3f2f.tar.gz";
+  sha256 = "0xkbvl1gqm1s5s2cfd47bbhd0hm3gws1f2n0n2gnsm8grv60wsx1";
 }) {
   config = { allowUnfree = true; };
   system = stdenv.system;
