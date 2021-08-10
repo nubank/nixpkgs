f5a7abeea9ec6aad6977552d447491febb9599f0
Eric Dallo <ericdallo06@hotmail.com>
2021-08-10 14:09:31 -0300

    Bump nixpkgs source

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,8 +1,8 @@
 { stdenv, ... }:
 
 import (builtins.fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/1441fa74d213d7cc120d9d7d49e540c1fc59bc58.tar.gz";
-  sha256 = "152qb7ch0r4bidik33zd0a9wl0929zr0dqs5l5ksm7vh3assc7sc";
+  url = "https://github.com/nixos/nixpkgs/archive/ebc72941f0d47625a03bac63f4b0ae55a356a849.tar.gz";
+  sha256 = "0q4r33kzhsdf624c8cxwip8xnhn3gs2i36jgw2q4cdmsjmyw8z5n";
 }) {
   config = { allowUnfree = true; };
   system = stdenv.system;
