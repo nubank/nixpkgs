a86725d254854fd5d359bc359ab38e55b36eb269
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2022-01-03 13:14:07 -0300

    Update nodejs from 10_x to 14_x, use terraform 1.0.2

--- default.nix
+++ default.nix
@@ -15,11 +15,17 @@ let
 
   hover = unstable.hover.override { inherit (final.nubank) flutter; };
 
-  nodejs = unstable.nodejs-10_x;
+  nodejs = unstable.nodejs-14_x;
 
   yarn = (unstable.yarn.override { inherit (final.nubank) nodejs; });
 
   leiningen = (unstable.leiningen.override { jdk = unstable.openjdk11; });
+
+  terraform = (unstable.mkTerraform {
+    version = "1.0.2";
+    sha256 = "sha256-KmSkHJ5RB2cP4z/IT/p+LHm658soM0IS0TPYKxU02z4=";
+    vendorSha256 = "sha256-s5HFBPWv7OEvgMBkAVZNivk6tD1T3QjUDqsIU9TPLmA=";
+  });
 in
 {
   nubank = {
@@ -46,7 +52,7 @@ in
       python38Full
       sassc
       tektoncd-cli
-      terraform_1_0
+      terraform
       yarn
       # TODO: ruby is installed by Ansible, but I never saw it used in Nubank
       # ruby
